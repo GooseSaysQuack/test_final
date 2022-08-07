698ce2d2ca9b7604ef116a208a643500384e34ac
GooseSaysQuack <goosesaysquack@gmail.com>
2022-08-07 17:43:02 +0600

    commit2

--- market/urls.py
+++ market/urls.py
@@ -1,13 +1,14 @@
 from rest_framework.routers import DefaultRouter
 
 from market.views import AuthorAPIView, GenreAPIView, BookListAPIView
+from market.models import Book
 
 router = DefaultRouter()
 
 # router.register('book', BookViewSet)
-router.register('book', BookListAPIView)
-router.register('author', AuthorAPIView)
-router.register('genre', GenreAPIView)
+router.register(r'book', BookListAPIView, basename='book')
+router.register(r'author', AuthorAPIView, basename='author')
+router.register('genre', GenreAPIView, basename='genre')
 
 urlpatterns = []
 
