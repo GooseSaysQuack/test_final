b8ba60159254b63b71483ce64c4af96c21461dbd
GooseSaysQuack <losev.danil@gmail.com>
2022-08-10 20:26:00 +0600

    we

--- shop/urls.py
+++ shop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include, re_path,
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from market.views import AuthorAPIView, BookListAPIView, GenreAPIView
 from rest_framework_swagger.views import get_swagger_view
